3e807c9fb8e684e19e9a17cdfea7c140bc5428da
Simone <coli.simone001@gmail.com>
2022-01-07 17:32:11 +0100

    final result

--- script/analysis.C
+++ script/analysis.C
@@ -31,31 +31,41 @@ void Checks() {
 		cout<<"----------------------------------\n";
 	}
 
+	TCanvas* can = new TCanvas("can", "Histograms");
+	can->Divide(2,2);
+
+	can->cd(1);
+	type->SetLineWidth(3);
+	type->SetFillColorAlpha(kBlue, .5);
+	type->Draw();
+	type->GetXaxis()->SetTitle("Particle Type");
+	type->GetYaxis()->SetTitle("Number of Particles");
+
 	// CCheck of the shape of the momentum ///////
-	TCanvas* c1 = new TCanvas("c1", "Momento");
+	can->cd(2);
 	TH1F* Mom = (TH1F*)c->Get("momentum");
 	Mom->Fit("expo","","");
 	Mom->SetFillColorAlpha(kBlue, 0.3);
 	Mom->SetLineWidth(6);
+	Mom->GetXaxis()->SetTitle("Momentum");
+	Mom->GetYaxis()->SetTitle("Occurrencies");
 	Mom->Draw();
-	c1->Print("../fit/Momentumfit.pdf");
 	gPad->Update();
 	TPaveStats* ft = (TPaveStats*)Mom->FindObject("stats");
 	ft->SetOptFit(optarg);
 	cout<<"----------------------------------\n";
-	cout<<Mom->GetMean()<<" - "<<Mom->GetRMS()<<" - "<< 1<<'\n';
+	cout<<Mom->GetMean()<<" - "<<Mom->GetRMS()<<" - "<<Mom->GetMeanError()<<" - "<< 1<<'\n';
 	cout<<"----------------------------------\n";
 
 	// Cheack of the angles ////////////////////
-	TCanvas* c2 = new TCanvas("c2", "Angoli");
-	c2->Divide(1,2);
-
-	c2->cd(1);
+	can->cd(3);
 	TH1F* Pol = (TH1F*)c->Get("polar");
 	Pol->Fit("pol0");
 	TF1* f1 = Pol->GetFunction("pol0");
 	f1->SetLineColor(kBlack);
 	Pol->SetLineColor(kRed);
+	Pol->GetXaxis()->SetTitle("Radiants");
+	Pol->GetYaxis()->SetTitle("Occurrences");
 	Pol->Draw();
 	gPad->Update();
 	TPaveStats* ft1 = (TPaveStats*)Pol->GetListOfFunctions()->FindObject("stats");
@@ -63,17 +73,19 @@ void Checks() {
 	gStyle->SetStatW(.2);
 	gStyle->SetStatH(.4);
 
-	c2->cd(2);
+	can->cd(4);
 	TH1F* azim = (TH1F*)c->Get("azim");
 	azim->Fit("pol0");
 	TF1* f2 = azim->GetFunction("pol0");
 	f2->SetLineColor(kBlack);
 	azim->SetLineColor(kBlue);
+	azim->GetXaxis()->SetTitle("Radiants");
+	azim->GetYaxis()->SetTitle("Occurrences");
 	azim->Draw();
 	gPad->Update();
 	TPaveStats* ft5 = (TPaveStats*)azim->FindObject("stats");
 	ft5->SetOptFit(optarg);
-	c2->Print("../fit/anglesfit.pdf");
+	can->Print("../fit/anglesfit.pdf");
 
 	// Check the entrance for each histogram
 
@@ -127,45 +139,51 @@ void Checks() {
 
 void analysis() {
 	int optarg = 1111;
-	TColor* myYellow = gROOT->GetColor(10);
+
 	TFile* f = new TFile("Alice_Simulation.root","read");
 
-	TCanvas* c3 = new TCanvas("c3", "Cariche");
+	TCanvas* can = new TCanvas("cam", "Fits");
+	can->Divide(2,2);
+	can->cd(1);
 	TH1F* Im1 = (TH1F*)f->Get("invmassdis");
 	TH1F* Im2 = (TH1F*)f->Get("invmasscon");
 	TH1F* diff1 = new TH1F("diff1","Difference 1", 1000, 0, 5);
 	diff1->Add(Im1,Im2, -1, 1);
 	diff1->Fit("gaus","","",0.5, 1.5);
 	diff1->SetAxisRange(0.5, 1.5);
+	diff1->GetXaxis()->SetTitle("Invariant Mass (Kg)");
+	diff1->GetYaxis()->SetTitle("Occurrences");
 	diff1->Draw();
 	gPad->Update();
 	TPaveStats* ft2 = (TPaveStats*)diff1->FindObject("stats");
-	c3->Print("../fit/Cariche_fit.pdf");
 	ft2->SetOptFit(optarg);
 	ft2->SetOptStat(10);
 
-	TCanvas* c4 = new TCanvas("c4", "Pioni e Kaoni");
+	can->cd(2);
 	TH1F* Im3 = (TH1F*)f->Get("invppkmpmkp");
 	TH1F* Im4 = (TH1F*)f->Get("invppkppmkm");
 	TH1F* diff2 = new TH1F("diff2","Difference 2", 1000, 0, 5);
 	diff2->Add(Im3,Im4, 1, -1);
 	diff2->Fit("gaus","","",0.7, 1.5);
 	diff2->SetAxisRange(0.7, 1.5);
+	diff2->GetXaxis()->SetTitle("Invariant Mass (Kg)");
+	diff2->GetYaxis()->SetTitle("Occurrences");
 	diff2->Draw();
 	gPad->Update();
 	TPaveStats* ft3 = (TPaveStats*)diff2->FindObject("stats");
-	c4->Print("../fit/pion_kaon_fit.pdf");
 	ft3->SetOptFit(optarg);
 	ft3->SetOptStat(10);
 
-	TCanvas* c5 = new TCanvas("c5", "Decadimento Kaoni");
+	can->cd(3);
 	TH1F* kaon = (TH1F*)f->Get("decay");
 	kaon->Fit("gaus","","",0.5, 1.25);
 	kaon->SetAxisRange(0.5, 1.5);
+	kaon->GetXaxis()->SetTitle("Invariant Mass (Kg)");
+	kaon->GetYaxis()->SetTitle("Occurrences");
 	kaon->Draw();
 	gPad->Update();
 	TPaveStats* ft4 = (TPaveStats*)kaon->FindObject("stats");
 	ft4->SetOptFit(optarg);
-	c5->Print("../fit/decay_fit.pdf");
+	can->Print("../fit/decay_fit.pdf");
 	ft4->SetOptStat(2211);
 }
